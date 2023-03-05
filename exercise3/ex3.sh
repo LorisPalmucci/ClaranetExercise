4cbce13f87a6bfb087638d5f6046ee50d33210d1
LorisPalmucci <64377102+LorisPalmucci@users.noreply.github.com>
2023-03-05 18:47:36 +0100

    Add files via upload

---
+++ exercise3/ex3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+crontab -l > /home/user/newCrontabJob
+
+echo "* * * * * tar -czvf /home/user/folder.tar.gz 
+    scp -i /percorso della chiave privata /path/to/archive.tar.gz 
+    user@remote_host:/path/on/remote_host" >> /home/user/newCrontabJob
+
+crontab /home/user/newCrontabJob
+
+rm /home/user/newCrontabJob
