4c73b26c3dd2a86f999177108d53ac675fe0f0d4
UncleJason88 <loris.palmucci@studenti.unicam.it>
2023-03-06 18:11:10 +0100

    add commente ex.3

--- task/exercise3/ex3.sh
+++ task/exercise3/ex3.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
+# crea un nuovo file nella specifica directory in cui verrà memorizzata una copia dei job pianificati
 crontab -l > /home/user/newCrontabJob
 
-echo "* * * * * tar -czvf /home/user/folder.tar.gz 
-    scp -i /percorso della chiave privata /path/to/archive.tar.gz 
-    user@remote_host:/path/on/remote_host" >> /home/user/newCrontabJob
+# 1. Imposta come tempo di esecuzione del job, ogni domenica sera alle ore 21.00 
+# 2. Usa il comando 'tar' per creare un archivio della directory.
+#       c --> crea il nuovo archivio
+#       z --> è il metodo di compressione
+#       v --> aggiunge ogni elemento presente nella cartella all'archio
+#       f --> specifica il nome dell'archivio
+# 3. Usa il comando scp -i per copiare l'archivio compresso sulla directory del server remoto
+#    usando la chiave privata contenuta nella specifica directory
+# 4. Infine salva l'intera stringa in coda al file precedentemente creato
+echo "0 21 * * 0 
+      tar -czvf /home/user/folder.tar.gz 
+      scp -i /percorso della chiave privata 
+             /path/to/archive.tar.gz user@192.168.1.100:/path/on/remote_host" >> /home/user/newCrontabJob
 
+# Il file viene messo nel CronTab del sistema con il nuovo job pianificato
 crontab /home/user/newCrontabJob
 
+# Rimuove la copia di 'newCrontabJob' precedentemente creata
 rm /home/user/newCrontabJob
